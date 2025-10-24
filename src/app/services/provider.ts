7dc86b6dba0043ceb8fd69ac97ff0e1bad6c8d3f
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-10-24 15:48:45 +0100

    ajout de provider

--- src/app/services/provider.ts
+++ src/app/services/provider.ts
@@ -6,12 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class Provider {
   
+  
     constructor(public http:HttpClient) //injection de dépendence
   {
     
   }
-
    getProviders(){
     return this.http.get("http://127.0.0.1:8081/providers/");
   }
+
+  saveProvider(provider:any){
+    return this.http.post("http://127.0.0.1:8081/providers/",provider);
+  }
+
 }
